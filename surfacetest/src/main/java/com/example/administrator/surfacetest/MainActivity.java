5bec65df439b2bcea20037cc4aed6d283ad1926d
Administrator <631706099@qq.com>
2016-03-06 22:11:55 +0800

    init

---
+++ surfacetest/src/main/java/com/example/administrator/surfacetest/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.administrator.surfacetest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    Button btn_1;
+    Button btn_2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+
+    }
+
+    private void init() {
+        btn_1 = (Button) findViewById(R.id.btn_1);
+        btn_2 = (Button) findViewById(R.id.btn_2);
+
+        btn_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Main1Activity.class));
+            }
+        });
+
+
+        btn_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Main2Activity.class));
+            }
+        });
+    }
+
+}
